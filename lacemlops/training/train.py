6f0363aa049fcbb6ec26bdb3d4fd40dd4c2dcecd
Lace Lofranco <lace.lofranco@microsoft.com>
2020-03-11 16:38:09 +1000

    remove early stopping

--- lacemlops/training/train.py
+++ lacemlops/training/train.py
@@ -26,8 +26,7 @@ def train_model(data, parameters):
     model = lightgbm.train(parameters,
                            train_data,
                            valid_sets=valid_data,
-                           num_boost_round=500,
-                           early_stopping_rounds=20)
+                           num_boost_round=500)
     return model
 
 
